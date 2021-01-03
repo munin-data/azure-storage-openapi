b99127272e14aba4d6e8f3b726f740a12a8d4af1
Nicholas Fish <github@seaofdirac.net>
2021-01-03 12:31:28 +0100

    Add support for path parameters containing paths to root POST path operation

--- app/main.py
+++ app/main.py
@@ -5,10 +5,10 @@ from fastapi import FastAPI, Response, status
 app = FastAPI()
 
 
-@app.post("/", status_code=status.HTTP_201_CREATED)
-async def write_json(response: Response):
+@app.post("/{directory:path}", status_code=status.HTTP_201_CREATED)
+async def write_json(directory: str, response: Response):
     response.headers["Location"] = "TODO"
-    return "Created"
+    return directory
 
 @app.get("/")
 def read_root():
